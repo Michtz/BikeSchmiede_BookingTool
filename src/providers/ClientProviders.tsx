2289beb49cf426eaec9519f4b9b8a3fa768b26d5
Michael Venetz <105941997+Michtz@users.noreply.github.com>
2026-02-21 21:58:29 +0100

    refactored for ssr and updated to all npm packages to lst version

---
+++ src/providers/ClientProviders.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+import { ErrorProvider } from '@/hooks/ErrorHook';
+import { CookieProvider } from '@/hooks/CookieHook';
+import { FeedbackProvider } from '@/hooks/FeedbackHook';
+import { SideCartProvider } from '@/hooks/SideCartHook';
+import SWRProvider from '@/providers/SWRProvider';
+import Feedback from '@/components/system/feedback/Feedback';
+import CookieBanner from '@/components/system/cookieBannner/CookieBanner';
+
+interface ClientProvidersProps {
+  children: ReactNode;
+}
+
+export const ClientProviders: React.FC<ClientProvidersProps> = ({ children }) => {
+  return (
+    <ErrorProvider>
+      <CookieProvider>
+        <FeedbackProvider>
+          <SideCartProvider>
+            <SWRProvider>
+              {children}
+              <Feedback />
+              <CookieBanner />
+            </SWRProvider>
+          </SideCartProvider>
+        </FeedbackProvider>
+      </CookieProvider>
+    </ErrorProvider>
+  );
+};
